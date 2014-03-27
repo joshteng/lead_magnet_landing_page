74f31b4395f0d2b4610a74128b2a39d3e5c582a6
Josh Teng <joshteng@webermize.com>
2014-03-27 17:02:21 +0800

    set up test configuration

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,18 @@ module EbookLandingPage
     I18n.enforce_available_locales = false
     
     # Test framework
-    config.generators.test_framework :rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
+
+
   end
 end
