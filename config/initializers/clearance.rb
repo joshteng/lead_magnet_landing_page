84ec7d430b239ff80c70d37d4fde770fa24feb14
Josh Teng <joshteng@webermize.com>
2014-03-27 20:07:06 +0800

    env variables and clearance config

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,8 @@
 Clearance.configure do |config|
-  config.mailer_sender = 'josh@growthautomator.com'
+  config.mailer_sender = ''
+  config.mailer_sender = ENV["ADMIN_EMAIL"]
+  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  config.cookie_path = '/'
+  config.password_strategy = Clearance::PasswordStrategies::BCrypt
+  config.allow_sign_up = true
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,17 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-def secure_token
-  token_file = Rails.root.join('.secret')
-  if File.exist?(token_file)
-    # Use the existing token.
-    File.read(token_file).chomp
-  else
-    # Generate a new token and store it in token_file.
-    token = SecureRandom.hex(64)
-    File.write(token_file, token)
-    token
-  end
-end
 
-EbookLandingPage::Application.config.secret_key_base = secure_token
+EbookLandingPage::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
